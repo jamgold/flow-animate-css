0cf6f38f850bae86ccaa53e047e599ea5f9986df
Jan Hendrik Mangold <jhm@cotren.net>
2018-02-03 11:40:58 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Small module that works with ostrio:flow-router-extra.
+
+It overrides two methods on the client to wait for animation to finish before leaving a page.
+
+Example:
+```
+<template name="pageOne">
+	<div class="animated fadeIn" data-animation-out="fadeOut">
+		<h1>Page One</h1>
+	</div>
+</template>
+
+<template name="pageTwo">
+	<div class="animated lightSpeedIn" data-animation-out="lightSpeedOut">
+		<h1>Page One</h1>
+	</div>
+</template>
+```
---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'jamgold:flow-animate-css',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Simple module to wait for animate.css animations before changing routes',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/jamgold/flow-animate-css.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.6');
+  api.use('ecmascript');
+  api.imply('ostrio:flow-router-extra');
+  api.mainModule('flow-animate-css.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('flow-animate-css');
+  api.mainModule('flow-animate-css-tests.js');
+});
